1b9aaf9be08ef781c80e9192bb4104dfd4e871e0
darshandv <darshandv10@gmail.com>
2018-03-19 17:00:43 +0530

    Discuss segment

--- app/src/main/java/com/myapp/darshan/sip/Discuss.java
+++ app/src/main/java/com/myapp/darshan/sip/Discuss.java
@@ -1,22 +1,34 @@
 package com.myapp.darshan.sip;
 
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class Discuss extends Fragment {
+    private Context mContext;
 
+    RelativeLayout mRelativeLayout;
+    private RecyclerView mRecyclerView;
 
-    public Discuss() {
-        // Required empty public constructor
-    }
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    String[] subject = {
+            "DC","SE","OS","DAA","SP","OOP"
+    };
+
 
 
     @Override
@@ -24,6 +36,23 @@ public class Discuss extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_discuss, container, false);
+
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mContext = getContext();
+        mRelativeLayout = (RelativeLayout) view.findViewById(R.id.rl);
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
+        mLayoutManager = new GridLayoutManager(mContext,2);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // Initialize a new instance of RecyclerView Adapter instance
+        mAdapter = new Adapter(mContext,subject);
+
+        // Set the adapter for RecyclerView
+        mRecyclerView.setAdapter(mAdapter);
+    }
 }
