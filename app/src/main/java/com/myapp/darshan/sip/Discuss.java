6c6127fc6f25b59435e0b3489e0f3fa058ca35a1
16CO216 <darshandv10@gmail.com>
2018-03-29 23:46:15 +0530

    Discuss segment basics

--- app/src/main/java/com/myapp/darshan/sip/Discuss.java
+++ app/src/main/java/com/myapp/darshan/sip/Discuss.java
@@ -2,11 +2,17 @@ package com.myapp.darshan.sip;
 
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,16 +25,16 @@ import android.widget.RelativeLayout;
 public class Discuss extends Fragment {
     private Context mContext;
 
-    RelativeLayout mRelativeLayout;
-    private RecyclerView mRecyclerView;
-
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager mLayoutManager;
+//    RelativeLayout mRelativeLayout;
+//    private RecyclerView mRecyclerView;
+//
+//    private RecyclerView.Adapter mAdapter;
+//    private RecyclerView.LayoutManager mLayoutManager;
+    private CardView c;
 
     String[] subject = {
-            "DC","SE","OS","DAA","SP","OOP"
+            "DC", "SE", "OS", "DAA", "SP", "OOP"
     };
-
 
 
     @Override
@@ -44,15 +50,25 @@ public class Discuss extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         mContext = getContext();
-        mRelativeLayout = (RelativeLayout) view.findViewById(R.id.rl);
-        mRecyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
-        mLayoutManager = new GridLayoutManager(mContext,2);
-        mRecyclerView.setLayoutManager(mLayoutManager);
+        c = (CardView)view.findViewById(R.id.cardDC);
+//        mRelativeLayout = (RelativeLayout) view.findViewById(R.id.rl);
+//        mRecyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
+//        mLayoutManager = new GridLayoutManager(mContext,2);
+//        mRecyclerView.setLayoutManager(mLayoutManager);
+//        mRecyclerView.addItemDecoration(new GridSpacingItemDecoration(2, dpToPx(10), true));
+//        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+//
+//        // Initialize a new instance of RecyclerView Adapter instance
+//        mAdapter = new Adapter(mContext,subject);
+//
+//        // Set the adapter for RecyclerView
+//        mRecyclerView.setAdapter(mAdapter);
+        c.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                c.getContext().startActivity(new Intent(c.getContext(),DCforum.class));
+            }
+        });
 
-        // Initialize a new instance of RecyclerView Adapter instance
-        mAdapter = new Adapter(mContext,subject);
-
-        // Set the adapter for RecyclerView
-        mRecyclerView.setAdapter(mAdapter);
     }
 }
