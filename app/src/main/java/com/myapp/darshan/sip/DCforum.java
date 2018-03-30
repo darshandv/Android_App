496feb9efee06792c233c17910e6755c0c436135
16CO216 <darshandv10@gmail.com>
2018-03-30 20:03:02 +0530

    Message sent to db from a forum

--- app/src/main/java/com/myapp/darshan/sip/DCforum.java
+++ app/src/main/java/com/myapp/darshan/sip/DCforum.java
@@ -1,7 +1,12 @@
 package com.myapp.darshan.sip;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.firebase.database.FirebaseDatabase;
 
 /**
  * Created by darshan on 29/3/18.
@@ -12,5 +17,30 @@ public class DCforum extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dc_forum);
+
+        FloatingActionButton fab =
+                (FloatingActionButton)findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText input = (EditText)findViewById(R.id.input);
+
+
+                FirebaseDatabase.getInstance()
+                        .getReference()
+                        .child("DCForum")
+                        .push()
+                        .setValue(new ChatMessage(input.getText().toString(),
+                                /*FirebaseAuth.getInstance()
+                                        .getCurrentUser()
+                                        .getDisplayName()*/"Darshan now")
+                        );
+
+                // Clear the input
+                input.setText("");
+            }
+        });
     }
+
 }
--- app/src/main/java/com/myapp/darshan/sip/Discuss.java
+++ app/src/main/java/com/myapp/darshan/sip/Discuss.java
@@ -66,5 +66,6 @@ public class Discuss extends Fragment {
             }
         });
 
+
     }
 }
