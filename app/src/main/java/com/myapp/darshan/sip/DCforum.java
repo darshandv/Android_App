0450b2cf69e1fc2e106c333a05764bee80d69f0c
16CO216 <darshandv10@gmail.com>
2018-03-30 20:45:44 +0530

    Testing retrival

--- app/src/main/java/com/myapp/darshan/sip/DCforum.java
+++ app/src/main/java/com/myapp/darshan/sip/DCforum.java
@@ -3,8 +3,11 @@ package com.myapp.darshan.sip;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.text.format.DateFormat;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -13,6 +16,8 @@ import com.google.firebase.database.FirebaseDatabase;
  */
 
 public class DCforum extends AppCompatActivity{
+
+    private FirebaseListAdapter<ChatMessage> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,6 +46,30 @@ public class DCforum extends AppCompatActivity{
                 input.setText("");
             }
         });
+
+
+        ListView listOfMessages = (ListView)findViewById(R.id.list_of_messages);
+
+        adapter = new FirebaseListAdapter<ChatMessage>(this, ChatMessage.class,
+                R.layout.message, FirebaseDatabase.getInstance().getReference().child("DCforum")) {
+            @Override
+            protected void populateView(View v, ChatMessage model, int position) {
+                // Get references to the views of message.xml
+                TextView messageText = (TextView)v.findViewById(R.id.message_text);
+                TextView messageUser = (TextView)v.findViewById(R.id.message_user);
+                TextView messageTime = (TextView)v.findViewById(R.id.message_time);
+
+                // Set their text
+                messageText.setText(model.getMessageText());
+                messageUser.setText(model.getMessageUser());
+
+                // Format the date before showing it
+                messageTime.setText(DateFormat.format("dd-MM-yyyy (HH:mm:ss)",
+                        model.getMessageTime()));
+            }
+        };
+
+        listOfMessages.setAdapter(adapter);
     }
 
 }
