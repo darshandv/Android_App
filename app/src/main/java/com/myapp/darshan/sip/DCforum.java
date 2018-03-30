be964f2be2c50224a4aec0a626a556c21cc60972
16CO216 <darshandv10@gmail.com>
2018-03-31 01:43:31 +0530

    Solving bugs to retrieve message

--- app/src/main/java/com/myapp/darshan/sip/DCforum.java
+++ app/src/main/java/com/myapp/darshan/sip/DCforum.java
@@ -5,11 +5,19 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.text.format.DateFormat;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 /**
  * Created by darshan on 29/3/18.
@@ -17,7 +25,14 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class DCforum extends AppCompatActivity{
 
-    private FirebaseListAdapter<ChatMessage> adapter;
+    //private FirebaseListAdapter<ChatMessage> adapter;
+    ArrayList<ChatMessage> arrayList;
+    DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference().child("DCforum");
+    final ListView listOfMessages = (ListView)findViewById(R.id.list_of_messages);
+
+    final TextView messageText = (TextView)findViewById(R.id.message_text);
+    final TextView messageUser = (TextView)findViewById(R.id.message_user);
+    final TextView messageTime = (TextView)findViewById(R.id.message_time);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,28 +63,54 @@ public class DCforum extends AppCompatActivity{
         });
 
 
-        ListView listOfMessages = (ListView)findViewById(R.id.list_of_messages);
 
-        adapter = new FirebaseListAdapter<ChatMessage>(this, ChatMessage.class,
-                R.layout.message, FirebaseDatabase.getInstance().getReference().child("DCforum")) {
+        mDatabase.addValueEventListener(new ValueEventListener() {
             @Override
-            protected void populateView(View v, ChatMessage model, int position) {
-                // Get references to the views of message.xml
-                TextView messageText = (TextView)v.findViewById(R.id.message_text);
-                TextView messageUser = (TextView)v.findViewById(R.id.message_user);
-                TextView messageTime = (TextView)v.findViewById(R.id.message_time);
+            public void onDataChange(DataSnapshot dataSnapshot) {
 
-                // Set their text
-                messageText.setText(model.getMessageText());
-                messageUser.setText(model.getMessageUser());
 
-                // Format the date before showing it
-                messageTime.setText(DateFormat.format("dd-MM-yyyy (HH:mm:ss)",
+                arrayList.clear();
+                for (DataSnapshot messageSnapshot : dataSnapshot.getChildren()){
+                    //String message = messageSnapshot.getValue(String.class);
+                    ChatMessage model = messageSnapshot.getValue(ChatMessage.class);
+                    messageText.setText(model.getMessageText());
+                    messageUser.setText(model.getMessageUser());
+                    messageTime.setText(DateFormat.format("dd-MM-yyyy (HH:mm:ss)",
                         model.getMessageTime()));
-            }
-        };
+//                    if (model==null ){
+//                        Toast.makeText(getApplicationContext(), "Null Message detected :", Toast.LENGTH_SHORT).show();
+//                        return;
+//                    }
+//
+                    arrayList.add(model);
 
-        listOfMessages.setAdapter(adapter);
+                }
+                ArrayAdapter<ChatMessage> arrayAdapter= new ArrayAdapter<ChatMessage>(getApplicationContext(), R.layout.message,arrayList);
+                listOfMessages.setAdapter(arrayAdapter);
+
+//            protected void populateView(View v, ChatMessage model, int position) {
+//                // Get references to the views of message.xml
+//                TextView messageText = (TextView)v.findViewById(R.id.message_text);
+//                TextView messageUser = (TextView)v.findViewById(R.id.message_user);
+//                TextView messageTime = (TextView)v.findViewById(R.id.message_time);
+//
+//                // Set their text
+//                messageText.setText(model.getMessageText());
+//                messageUser.setText(model.getMessageUser());
+//
+//                // Format the date before showing it
+//                messageTime.setText(DateFormat.format("dd-MM-yyyy (HH:mm:ss)",
+//                        model.getMessageTime()));
+//            }
+//
+//        listOfMessages.setAdapter(adapter);
     }
 
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+
+        });
+    }
 }
